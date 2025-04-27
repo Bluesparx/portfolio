f1270953cfcd4a12b47ee8bce4a2c5ca26d0f2c0
Bluesparx <nazia.hassan2807@gmail.com>
2025-04-27 12:54:16 +0530

    edit resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,8 +7,21 @@ const Resume = () => {
                 <div className="flex flex-col">
                     <h2 className='pb-6 text-xl font-bold tracking-right dark:text-gray-100 text-gray-800'>Resume</h2>
                     <div className="flex space-x-4">
-                        <a className="inline-block px-4 py-2 bg-gray-100 rounded-lg  dark:bg-opacity-75 dark:text-gray-950 dark:bg-gray-300 hover:dark:bg-opacity-100 hover:bg-gray-200 transition" href="./NaziaHassan.pdf" target="_blank" rel="noopener noreferrer">View</a>
-                        <a className="inline-block px-4 py-2 bg-gray-100 rounded-lg dark:bg-opacity-75 dark:text-gray-950 dark:bg-gray-300 hover:dark:bg-opacity-100 hover:bg-gray-200 transition" href="./NaziaHassan.pdf" download>Download</a>
+                    <a
+                        href="/NaziaHassan.pdf"
+                        target="_blank"
+                        rel="noopener noreferrer" // this is used for view only feature
+                        className="inline-block px-4 py-2 bg-gray-100 rounded-lg dark:bg-opacity-75 dark:text-gray-950 dark:bg-gray-300 hover:dark:bg-opacity-100 hover:bg-gray-200 transition"
+                    >
+                        View
+                    </a>
+                    <a
+                        href="/NaziaHassan.pdf" 
+                        download
+                        className="inline-block px-4 py-2 bg-gray-100 rounded-lg dark:bg-opacity-75 dark:text-gray-950 dark:bg-gray-300 hover:dark:bg-opacity-100 hover:bg-gray-200 transition"
+                    >
+                        Download
+                    </a>   
                     </div>
                 </div>
             </div>
